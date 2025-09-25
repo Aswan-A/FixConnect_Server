302d21322e7bfda2afb20a2a9378f430eaa27ebc
Aswan-A <aswan0366@gmail.com>
2025-09-26 00:35:22 +0530

    typo fix

--- src/models/Issue.ts
+++ src/models/Issue.ts
@@ -5,11 +5,24 @@ const issueSchema = new mongoose.Schema({
   description: { type: String, required: true },
   image: String,
   createdAt: { type: Date, default: Date.now },
-  location: {type:String,reuired:true},
-  status: { type: String, enum: ['open', 'in progress', 'resolved'], default: 'open' },
+  location: { type: String, required: true },
+  status: { 
+    type: String, 
+    enum: ['open', 'in progress', 'resolved'], 
+    default: 'open' 
+  },
   reportedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  category: { type: String, enum: ['Electronics', 'Electrical', 'Plumbing', 'other'], default: 'other' },
+  category: { 
+    type: String, 
+    enum: ['Electronics', 'Electrical', 'Plumbing', 'Other'], 
+    default: 'Other' 
+  },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  budget: { 
+    type: Number, 
+    min: 0, 
+    default: 0 
+  }
 });
 
 const Issue = mongoose.model('Issue', issueSchema, 'Issues');
